9293127d0658c5e41c0d3bf2e6998767e4d05075
treyshedrick <treyshedrick@gmail.com>
2021-01-02 21:05:04 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -3,21 +3,15 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="App container-fluid">
+      <header className="row">
+        <div className="col-1">
+          <img src={logo} className="App-logo" alt="logo" />
+        </div>
+        <div className="col-10 ReactTube">ReactTube</div>
+        <div className="col-1"></div>
       </header>
+
     </div>
   );
 }
