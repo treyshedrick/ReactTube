23fce0d73f331631252a5e22e25a280ff3197bb2
treyshedrick <treyshedrick@gmail.com>
2021-01-07 19:06:47 -0600

    main class component and implement axios

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,8 +1,43 @@
-let Main = () =>{
-    return(
-        <div className="container">
-            Test
-        </div>
-    )
+import React, { Component } from 'react';
+import axios from 'axios';
+
+class Main extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isLoaded: false,
+            data: {},
+            error: null
+        }
+    }
+
+    componentDidMount(){
+        axios.get('https://jsonplaceholder.typicode.com/todos/1')
+            .then(response=> {
+                this.setState({
+                    isLoaded: true,
+                    data: response.data})
+            })
+            .catch(axiosError =>{
+                this.setState({
+                    error: axiosError
+                })
+            })
+    }
+
+    render() { 
+        if(this.state.isLoaded){
+            console.log(this.state.data)
+            return(
+                <div className="container">
+                    <h1>{this.state.data.title}</h1>
+                </div>
+            );
+        }
+            return (
+            <div className="container">
+            </div> );
+    }
 }
-export default Main
+ 
+export default Main;
