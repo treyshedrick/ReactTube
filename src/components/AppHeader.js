95fba3afaa15b3c8fa8ee7664665daa88fa9c594
treyshedrick <treyshedrick@gmail.com>
2021-01-15 08:12:39 -0600

    update styling and youtube endpoint

--- src/components/AppHeader.js
+++ src/components/AppHeader.js
@@ -3,15 +3,15 @@ import logo from '../logo.svg';
 const AppHeader = () =>{
     return(
         <header className="row AppHeader">
-            <div className="col-1">
+            <div className="col-2">
                 <img src={logo} className="App-logo" alt="logo" />
                 <div className="ReactTube">ReactTube</div>
             </div>
-            <div className="col-10">
+            <div className="col-8">
                 <input type="text" className="form-control" value="ReactJS" readOnly></input>
                 <button className="btn btn-outline-secondary border-0 border header-button"><i className="fa fa-search"></i></button>
             </div>
-            <div className="col-1"></div>
+            <div className="col-2"></div>
       </header>
     )
 }
--- src/components/Main.js
+++ src/components/Main.js
@@ -16,7 +16,7 @@ class Main extends Component {
     
     componentDidMount(){
         //Youtube Endpoint
-        //axios.get('https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=12&order=viewCount&q=ReactJS%20Tutorial&key=' + process.env.REACT_APP_YOUTUBE_API_KEY)
+        //axios.get('https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=12&type=video&order=viewCount&q=ReactJS%20Tutorial&key=' + process.env.REACT_APP_YOUTUBE_API_KEY)
         axios.get('./test.json') //test file
             .then(response=> {
                 this.setState({
@@ -33,7 +33,7 @@ class Main extends Component {
     componentDidUpdate(prevProps){
         if(prevProps.urlSearch !== this.props.urlSearch){
             //Youtube Endpoint
-            //axios.get('https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=12&q=ReactJS%20Tutorial%20'+this.props.urlSearch+'&key=' + process.env.REACT_APP_YOUTUBE_API_KEY)
+            //axios.get('https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=12&type=video&q=ReactJS%20Tutorial%20'+this.props.urlSearch+'&key=' + process.env.REACT_APP_YOUTUBE_API_KEY)
             axios.get('./test2.json')
             .then(response=> {
                 this.setState({
@@ -53,7 +53,7 @@ class Main extends Component {
         const vidArr = this.state.data.items;
         if(this.state.isLoaded){
             return (
-                <div className="container">
+                <div className="container-fluid Main">
                     <div className="row">
                         {vidArr.slice(0,3).map((i) => <Videos videoItem = {i} key ={i.etag}/>)}
                     </div>
