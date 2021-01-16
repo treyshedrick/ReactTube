e8fa264e82c9f6a57b7bc61d2062d907e5d439e2
treyshedrick <treyshedrick@gmail.com>
2021-01-16 12:44:14 -0600

    initial Video Player logic

--- src/components/Main.js
+++ src/components/Main.js
@@ -9,8 +9,12 @@ class Main extends Component {
         this.state = {
             isLoaded: false,
             data: {},
-            error: null
+            error: null,
+            playVideo: false,
+            videoToPlay: null
         }
+
+        this.clickHandler = this.clickHandler.bind(this);
     }
 
     
@@ -18,42 +22,66 @@ class Main extends Component {
         //Youtube Endpoint
         //axios.get('https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=12&type=video&order=viewCount&q=ReactJS%20Tutorial&key=' + process.env.REACT_APP_YOUTUBE_API_KEY)
         getYoutubeContentMain(this,'./test.json')
+        document.addEventListener('touchstart', this.onTouchStart, { passive: true });
     }
 
     componentDidUpdate(prevProps){
         if(prevProps.urlSearch !== this.props.urlSearch){
             //Youtube Endpoint
             //axios.get('https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=12&type=video&q=ReactJS%20Tutorial%20'+this.props.urlSearch+'&key=' + process.env.REACT_APP_YOUTUBE_API_KEY)
+            this.setState({
+                playVideo: false
+            })
             getYoutubeContentMain(this,'./test2.json')
+            console.log("API CALL BECAUSE OF UPDATE TO COMPONENT")
         }
     }
 
-    render() { 
+    clickHandler = (video) =>{
+        this.setState({
+            playVideo: !this.state.playVideo,
+            videoToPlay: video
+        })
+    }
+
+    render() {
         const vidArr = this.state.data.items;
-        if(this.state.isLoaded){
+        if(this.state.isLoaded && this.state.playVideo === false){
             return (
                 <div className="container-fluid Main">
                     <div className="row">
-                        {vidArr.slice(0,3).map((i) => <Videos videoItem = {i} key ={i.etag}/>)}
+                        {vidArr.slice(0,3).map((i) => <div className="col-md-4 cursor" onClick={() => this.clickHandler(i)} key={i.etag}><Videos videoItem = {i} key ={i.etag}/></div>)}
                     </div>
                     <div className="row">
-                        {vidArr.slice(3,6).map((i) => <Videos videoItem = {i} key ={i.etag}/>)}
+                        {vidArr.slice(3,6).map((i) => <div className="col-md-4 cursor" onClick={() => this.clickHandler(i)} key={i.etag}><Videos videoItem = {i} key ={i.etag}/></div>)}
                     </div>
                     <div className="row">
-                        {vidArr.slice(6,9).map((i) => <Videos videoItem = {i} key ={i.etag}/>)}
+                        {vidArr.slice(6,9).map((i) => <div className="col-md-4 cursor" onClick={() => this.clickHandler(i)} key={i.etag}><Videos videoItem = {i} key ={i.etag}/></div>)}
                     </div>
                     <div className="row">
-                        {vidArr.slice(9,12).map((i) => <Videos videoItem = {i} key ={i.etag}/>)}
-                    </div>
+                        {vidArr.slice(9,12).map((i) => <div className="col-md-4 cursor" onClick={() => this.clickHandler(i)} key={i.etag}><Videos videoItem = {i} key ={i.etag}/></div>)}
+                    </div>                    
                 </div>
             );
-        } else if(this.state.error){
+        } else if(this.state.playVideo){
+            return (   
+                <div className="container-fluid Main">
+                    <button onClick={this.clickHandler} className="HTbuttons">back</button>
+                    <div className="embed-responsive embed-responsive-16by9">
+                        <iframe title={this.state.videoToPlay.id.videoId} className="embed-responsive-item" src={"https://youtube.com/embed/"+this.state.videoToPlay.id.videoId+"?autoplay=1"} allowFullScreen></iframe>
+                    </div>
+                    {"Test For Timeout"}
+                    </div>
+                
+            );
+        }else if(this.state.error){
             return (   
                 <Error error={this.state.error.message} />
             );
         } else{
             return (
                 <div className="container">test</div> );
+                
         }
     }
 }
--- src/components/Videos.js
+++ src/components/Videos.js
@@ -1,3 +1,5 @@
+import { Fragment } from "react";
+
 const Videos = (props) =>{
     const video = props.videoItem;
     
@@ -28,14 +30,12 @@ const Videos = (props) =>{
     }
 
     return(
-        <div className="col-md-4">
-            <a href={"https://youtube.com/watch?v="+video.id.videoId} className="watchVideo">
-                <img src={video.snippet.thumbnails.medium.url} alt="videoimage" className="video img-fluid rounded"></img>
-                <div className="videoName">{videoTitle}</div>
-            </a>
+        <Fragment>
+            <img src={video.snippet.thumbnails.medium.url} alt="videoimage" className="video img-fluid rounded"></img>
+            <div className="videoName">{videoTitle}</div>
             <a href={"https://youtube.com/channel/"+video.snippet.channelId} className="channelName">{video.snippet.channelTitle}</a>
             <div className="uploaded">{whenUploaded}</div>
-        </div>
+        </Fragment>
     )
 }
 
